4f8624320dd5ef939de17844e6c227488420bb34
Dawid Lachor <dawidlachor@gmail.com>
2020-01-19 16:28:19 +0100

    modified index.php

--- menu.php
+++ menu.php
@@ -9,15 +9,19 @@ if ($polaczenie->connect_errno) {
     $pizza = 1;
     $zapytanie = $polaczenie->query(" select idPizza, NazwaPizzy from pizze where StandarPizza = 1;");
     while ($row = mysqli_fetch_array($zapytanie, MYSQLI_ASSOC)) {
-        echo "<div class='col-6'>";
+        echo "<div class='col-6'><h4>";
         echo $pizza;
         echo ". ";
-        echo $row['NazwaPizzy']."<br>";
+        echo $row['NazwaPizzy']."</h4>";
+        echo "<p class='ml-4'>Składniki: </p>";
+        echo "<p class='ml-5'>";
         $skladniki = $polaczenie->query("select dodatki.NazwaDodatku from dodatki inner join dod_pizz on dodatki.idDodatku = dod_pizz.idDodatku inner join pizze on pizze.idPizza = dod_pizz.idPizzy where dod_pizz.idPizzy =  ". $row['idPizza'] ." ;");
         while ($skl = mysqli_fetch_array($skladniki, MYSQLI_ASSOC)){
+
             echo $skl["NazwaDodatku"];
             echo ", ";
         }
+        echo "</p>";
         $skladniki->close();
         $pizza = $pizza + 1;
 
